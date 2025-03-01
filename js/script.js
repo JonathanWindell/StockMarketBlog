240ec60e723bb7ad137fd58f45fd3f253754d8b1
Jonathan Windell <windelljonathan04@gmail.com>
2025-03-01 14:26:03 +0100

    Commit, Fixat markdown filer

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
+window.onload = function() {
+  // Uppdatera datum och tid
+  function updateDateTime() {
+    var dt = new Date();
+    var options = { weekday: 'long', day: 'numeric', month: 'long' };
+    var formattedDate = dt.toLocaleDateString('sv-SE', options);
+    formattedDate = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    document.getElementById("dateTime").innerHTML = formattedDate;
+  }
+
+  updateDateTime();
+  setInterval(updateDateTime, 1000);
+
+  // Hämta senaste inlägget
+  function loadLatestPost() {
+    fetch('Posts/TestPost.md')  // Se till att filen finns i rätt mapp
+      .then(response => response.text())
+      .then(markdown => {
+        document.getElementById('latestPostContent').innerHTML = marked.parse(markdown);
+      })
+      .catch(error => console.error("Error loading post:", error));
+  }
+
+  loadLatestPost();
+};
+
+//Fixa artikelsidan med gamla. När ny post är uppladda så ska de bytas ut.
+
 
 
